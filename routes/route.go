04d9970e4941b725293f5a9eb0841167cc199ded
zhangyu <ylnwqm@163.com>
2022-07-28 09:02:46 +0800

    request routeing groups success

--- routes/route.go
+++ routes/route.go
@@ -2,21 +2,17 @@ package routes
 
 import (
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/core/router"
 )
 
 func InitApp(app *iris.Application) {
-	app.PartyFunc("/admin", func(r router.Party) {
-		/*r.Handle("GET", "/login", func(context iris.Context) {
-			context.JSON(iris.Map{"message": "pong"})
-		})*/
-
-		app.Get("/login", func(ctx iris.Context) {
+	//如果是闭包，则需要声明route，如果是模板，则需要声明Party,并在子路由使用iris.Party
+	app.PartyFunc("/admin", func(r iris.Party) {
+		r.Get("/login", func(ctx iris.Context) {
 			// 绑定： {{.message}}　为　"Hello world!"
 			println("就按就按家家爱")
-			ctx.ViewData("message", "Hello world!")
+			//ctx.ViewData("message", "Hello world!")
 			// 渲染模板文件： ./views/hello.html
-			ctx.View("hello.html")
+			//ctx.View("hello.html")
 		})
 	})
 }
