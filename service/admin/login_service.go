209304a7f6d63fc480858d89d8e5a62e15b31e6c
zhangyu <ylnwqm@163.com>
2022-08-01 18:06:30 +0800

    1. get data form service 2. add ignore

--- service/admin/login_service.go
+++ service/admin/login_service.go
@@ -1,9 +1,24 @@
 package admin
 
+import (
+	"fmt"
+	"go-hyper-blog/model"
+	"gorm.io/gorm"
+)
+
+/*type LoginService interface {
+	GetUser(username string, passowrd string)
+}*/
+
 type LoginService struct {
 	//里边是具体要实现的方法
+	DB *gorm.DB
 }
 
-func (l *LoginService) GetUser(username string, password string) {
+func (l *LoginService) GetUser(username string, password string) []model.Admin {
+	result := []model.Admin{}
 
+	l.DB.Raw("select * from admin").Scan(&result)
+	fmt.Println(result)
+	return result
 }
